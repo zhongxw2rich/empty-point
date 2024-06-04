8b619c72d0a01b7078e7ae410c4aa0c8c7270d99
zhongxw2rich <zhongxw2024@gmail.com>
2024-06-04 23:04:25 +0800

    前后端交互

--- next-js-app/src/app/page.tsx
+++ next-js-app/src/app/page.tsx
@@ -1,6 +1,13 @@
-import { useSession } from "next-auth/react";
-
+import axios from "axios";
 
 export default function Home() {
-  return <div><h1> Home Page</h1></div>;
+  const hello = axios.get('http://localhost:3000/api/fast-api-app/hello')
+    .then(response => JSON.stringify(response.data))
+    .catch( error => {
+        console.log(error)
+        return 'error'
+      }
+    );
+
+  return <div><h1> Home Page</h1><div>{hello}</div></div>;
 }
